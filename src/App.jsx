ac35f8274d2856be7e05e365ca9222e1d676c2b6
Verezhak <153289346+Verezhak@users.noreply.github.com>
2024-11-03 08:45:53 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+
+import 'modern-normalize';
+
+import { Suspense, lazy, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsRefreshing } from './redux/auth/selectors';
+import Layout from './components/Layout/Layout';
+import { refreshUser } from './redux/auth/operations';
+import Loader from './components/Loader/Loader';
+import { PrivateRoute } from './routes/PrivateRoute';
+import { RestrictedRoute } from './routes/RestrictedRoute';
+
+
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const ContactsPage = lazy(() => import('./pages/ContactsPage/ContactsPage'));
+const RegistrationPage = lazy(() => import('./pages/ RegistrationPage/RegistrationPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
+
+const App = () => {
+  const dispatch = useDispatch();
+  const { isRefreshing } = useSelector(selectIsRefreshing);
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+  return isRefreshing ? (
+    <Loader />
+  ) : (
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route
+            path="contacts"
+            element={<PrivateRoute>
+              <ContactsPage />
+            </PrivateRoute>}
+
+          />
+          <Route
+            path="register"
+            element={
+              <RestrictedRoute>
+                <RegistrationPage />
+              </RestrictedRoute>
+            }
+          />
+
+          <Route
+            path="login"
+            element={
+              <RestrictedRoute>
+                <LoginPage />
+              </RestrictedRoute>
+            }
+          />
+          <Route path='*' element={<NotFoundPage />} />
+        </Route>
+
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default App;
+
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom/client';
+import 'modern-normalize';
+import App from './App.jsx'
+import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store.js';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
+);
+
